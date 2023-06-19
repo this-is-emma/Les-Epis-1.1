73281e77ea899a03fd5ea368e26d07c9c33011f6
ThisIsEmma <sakatia.lise@gmail.com>
2023-06-18 20:52:13 -0400

    worked on pledges and look

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -9,50 +9,46 @@ class PledgesController < ApplicationController
   end
 
   def new
+    @campaigns = Campaign.all
     @pledge = Pledge.new
   end
 
   def create
+    @campaigns = Campaign.all
+
     @pledge = Pledge.new(pledge_params)
-    @pledge.user_id = current_user
+    @pledge.user_id = current_user.id
 
-    # if params[:pledge].key?(:campaigns)
-    #   @campaigns = Campaign.find(
-    #     params[:pledge][:categories]
-    #   )
-    #   @pledge.campa << @categories
-    # end
-
-    # if @pledge.save
-    #   redirect_to(pledge_path(@pledge))
-    # else
-    #   render(
-    #     :new,
-    #     status: :unprocessable_entity
-    #   )
-    # end
+    if @pledge.save
+      redirect_to(pledge_path(@pledge))
+    else
+      render(
+        :new,
+        status: :unprocessable_entity
+      )
+    end
   end
 
   def edit
     @pledge = Pledge.find(params[:id])
   end
 
-  def update
-    #
-    # @pledge = Pledge.find(params[:id])
+  # def update
 
-    # if @pledge.update(pledge_params)
-    #   if params[:pledge].key?(:categories)
-    #     @categories = Category.find(params[:pledge][:categories])
-    #     @pledge.categories.destroy_all
-    #     @pledge.categories << @categories
-    #   end
+  #   @pledge = Pledge.find(params[:id])
 
-    #   redirect_to(pledge_path(@pledge), flash: { success: "Pledge updated successfully"})
-    # else
-    #   render(:edit, status: :unprocessable_entity)
-    # end
-  end
+  #   if @pledge.update(pledge_params)
+  #     if params[:pledge].key?(:categories)
+  #       @categories = Category.find(params[:pledge][:categories])
+  #       @pledge.categories.destroy_all
+  #       @pledge.categories << @categories
+  #     end
+
+  #     redirect_to(pledge_path(@pledge), flash: { success: "Pledge updated successfully"})
+  #   else
+  #     render(:edit, status: :unprocessable_entity)
+  #   end
+  # end
 
   def destroy
     @pledge = Pledge.find(params[:id])
@@ -64,6 +60,6 @@ class PledgesController < ApplicationController
   private
 
   def pledge_params
-    params.require(:pledge).permit(:amount, :user_id, :campaign_id)
+    params.require(:pledge).permit(:amount, :campaign_id)
   end
 end
